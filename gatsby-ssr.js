4aeb87679204bc97b9f8cbe1ba6eb2aad30c0196
wiloke1 <hpvanlong@gmail.com>
2022-02-21 18:25:19 +0700

    test 8

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,2 +1,27 @@
-// import wrapWithProvider from './src/WrapWithProvider';
-// export const wrapRootElement = wrapWithProvider;
+import React from 'react';
+import { renderToString } from 'react-dom/server';
+import { RendererProvider } from 'react-fela';
+import { createRenderer } from 'fela';
+import { renderToSheetList } from 'fela-dom';
+import WrapWithProvider from './src/WrapWithProvider';
+
+exports.replaceRenderer = ({ bodyComponent, replaceBodyHTMLString, setHeadComponents }) => {
+  // let config = {
+  //   plugins: [kebabCase(), embedded(), multipleSelectors(), felaPlugin({ pixelToRem: false, rootFontSizePercent: -1, removeHoverOnMobile: true })],
+  //   enhancers: [],
+  // };
+  const bodyHTML = renderToString(<WrapWithProvider>{bodyComponent}</WrapWithProvider>);
+  // const sheetList = renderToSheetList(renderer)
+
+  // const elements = sheetList.map(({ type, css, media, support }) =>
+  //   <style
+  //     dangerouslySetInnerHTML={{ __html: css }}
+  //     data-fela-type={type}
+  //     data-fela-support={support}
+  //     key={`${type}-${media}`}
+  //     media={media}
+  //   />
+  // )
+  replaceBodyHTMLString(bodyHTML);
+  // setHeadComponents(elements)
+};
