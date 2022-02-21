696979fed037dd4e2fe87f5d7c2aec3f5328c9a4
wiloke1 <hpvanlong@gmail.com>
2022-02-21 17:40:51 +0700

    test 1

--- src/WrapWithProvider.tsx
+++ src/WrapWithProvider.tsx
@@ -1,10 +1,9 @@
-import { themeOverrides } from 'themeOverrides';
-import { ThemeProvider, useStyleSheet, View } from 'wiloke-react-core';
-import styleBase from 'styles/base';
 import { FC, ReactNode } from 'react';
+import styleBase from 'styles/base';
 import * as styles from 'styles/global';
+import { useStyleSheet, View } from 'wiloke-react-core';
 
-const CSSGlobal: FC = ({ children }) => {
+const _CSSGlobal: FC = ({ children }) => {
   const { renderer } = useStyleSheet();
 
   renderer.renderStatic(styleBase);
@@ -16,11 +15,12 @@ const WrapWithProvider: FC<{ element?: ReactNode }> = ({ element, children }) =>
   // Instantiating store in `wrapRootElement` handler ensures:
   //  - there is fresh store for each SSR page
   //  - it will be called only once in browser, when React mounts
-  return (
-    <ThemeProvider themeOverrides={{ ...themeOverrides, direction: 'ltr' }}>
-      <CSSGlobal>{element ?? children}</CSSGlobal>
-    </ThemeProvider>
-  );
+  return <div>{element ?? children}</div>;
+  // return (
+  //   <ThemeProvider themeOverrides={{ ...themeOverrides, direction: 'ltr' }}>
+  //     <CSSGlobal>{element ?? children}</CSSGlobal>
+  //   </ThemeProvider>
+  // );
 };
 
 export default WrapWithProvider;
