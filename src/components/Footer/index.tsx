9fabbdd3d9e61adbebea4a3c76c44c9f59df1650
wiloke1 <hpvanlong@gmail.com>
2022-03-03 15:07:32 +0700

    commit

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -15,11 +15,6 @@ export const Footer: FC = () => {
         </div>
       </div>
       <div className={styles.copyright}>Copyright © 2022.All Rights Reserved By Veda Builder</div>
-      <div className={styles.bg}>
-        <svg width="100%" viewBox="0 0 1440 712" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path d="M0 160.472C583.5 -66.0508 865.5 -40.5513 1440 160.472V711.449H0V160.472Z" fill="#030634" />
-        </svg>
-      </div>
     </footer>
   );
 };
