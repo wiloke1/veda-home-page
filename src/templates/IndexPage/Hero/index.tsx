e21ebd0d2d96c838ba747ae6c66f7eba3e6a117d
wiloke1 <hpvanlong@gmail.com>
2022-03-03 21:47:52 +0700

    commit

--- src/templates/IndexPage/Hero/index.tsx
+++ src/templates/IndexPage/Hero/index.tsx
@@ -8,9 +8,11 @@ export interface HeroProps extends SectionHero {}
 export const Hero: FC<HeroProps> = ({ heading, description }) => {
   return (
     <section className={styles.container}>
-      <h1 className={styles.heading} dangerouslySetInnerHTML={{ __html: heading }} />
-      <p className={styles.description}>{description}</p>
-      <GetStartedForm containerClassName={styles.form} />
+      <div className="container">
+        <h1 className={styles.heading} dangerouslySetInnerHTML={{ __html: heading }} />
+        <p className={styles.description}>{description}</p>
+        <GetStartedForm containerClassName={styles.form} />
+      </div>
     </section>
   );
 };
