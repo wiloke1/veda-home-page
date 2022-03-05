02fccb39db0ecdc342b4ba558e873c82e52dfeb9
wiloke1 <hpvanlong@gmail.com>
2022-03-05 11:05:10 +0700

    commit

--- src/templates/PricingPage/Faqs/index.tsx
+++ src/templates/PricingPage/Faqs/index.tsx
@@ -4,15 +4,17 @@ import { Section } from 'components/Section';
 import ReactMarkdown from 'react-markdown';
 import { IFaqs } from 'types/Pricing';
 import { Title } from 'components/Title';
+import { LinkButton } from 'components/LinkButton';
 import * as styles from './Faqs.module.scss';
 
 export const Faqs: FC<IFaqs> = ({ heading, description, decorate, collapse }) => {
   return (
-    <Section>
+    <Section style={{ overflow: 'hidden' }} backgroundColor="var(--color-gray1)">
       <div className="container">
         <div className="row">
           <div className="col-xs-12 col-sm-6 col-md-4">
             <Title title={heading} text={description} decorate={decorate} align="left" />
+            <LinkButton>Ask our agents</LinkButton>
           </div>
           <div className="col-xs-12 col-sm-6 col-md-8">
             {collapse.map((item, index) => {
