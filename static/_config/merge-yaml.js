e60ab74dead7ed1e886e387adfc7fb844dfee0c0
wiloke1 <hpvanlong@gmail.com>
2022-03-07 10:46:21 +0700

    commit

--- static/_config/merge-yaml.js
+++ static/_config/merge-yaml.js
@@ -5,9 +5,9 @@ const rootFile = path.resolve(__dirname, 'config.yml');
 const rootContent = fs.readFileSync(rootFile, 'utf8');
 const outputFile = path.resolve(__dirname, '../admin/config.yml');
 
-const result = rootContent.replace(/^.*@include.*/gm, match => {
-  const fileName = path.resolve(__dirname, `${match.replace(/.*@include\s*/g, '').trim()}.yml`);
-  const tab = match.match(/^\s*(?=@include)/gm)?.[0] ?? '';
+const result = rootContent.replace(/^.*\*include.*/gm, match => {
+  const fileName = path.resolve(__dirname, `${match.replace(/.*\*include\s*/g, '').trim()}.yml`);
+  const tab = match.match(/^\s*(?=\*include)/gm)?.[0] ?? '';
   const childContent = fs.readFileSync(fileName, 'utf8').replace(/^/gm, tab);
   return childContent;
 });
