8792aa24d16f6a6054838bff645d7209049dc018
wiloke1 <hpvanlong@gmail.com>
2022-03-10 17:50:07 +0700

    commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,8 +10,7 @@ const gatsbyRequiredRules = path.join(process.cwd(), 'node_modules', 'gatsby', '
 module.exports = {
   siteMetadata: {
     title: 'Veda builder',
-    description:
-      'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+    description: 'The All In One Store Design Solution',
   },
   plugins: [
     'gatsby-plugin-remove-trailing-slashes',
