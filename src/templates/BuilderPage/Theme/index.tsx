30a881aeb100d873a18dcda87a42feed8975ae48
wiloke1 <hpvanlong@gmail.com>
2022-04-08 10:11:21 +0700

    commit

--- src/templates/BuilderPage/Theme/index.tsx
+++ src/templates/BuilderPage/Theme/index.tsx
@@ -3,6 +3,7 @@ import { Section } from 'components/Section';
 import { Title } from 'components/Title';
 import { FC } from 'react';
 import { SectionThemes } from 'types/Builder';
+import { builderMode } from 'utils/builderMode';
 import { Content } from './Content';
 
 export interface ThemeProps extends SectionThemes {}
@@ -11,7 +12,7 @@ export const Theme: FC<ThemeProps> = ({ heading, description, decorate, backgrou
   return (
     <Section backgroundColor={backgroundColor} backgroundImage={backgroundImage}>
       <Title title={heading} text={description} decorate={decorate} />
-      {window.builderMode ? <NetlifyWarning pageName="theme" /> : <Content />}
+      {builderMode.get() ? <NetlifyWarning pageName="theme" /> : <Content />}
     </Section>
   );
 };
